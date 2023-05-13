721b8980e32d99622cae1c3ed558ff8d8198ef5b
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-13 21:46:39 +0300

    chore: add validator for input number

---
+++ Calculator/Models/NumberValidator.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calculator.Models;
+
+public static class NumberValidator
+{
+    public static bool ValidateNumber(string number, int numberSystem)
+    {
+        throw new NotImplementedException();
+    }
+}
