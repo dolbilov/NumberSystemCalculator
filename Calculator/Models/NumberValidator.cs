ff25e2a62f8eea3eac4ae2ac40d3392419014803
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-13 22:32:48 +0300

    chore: finish number validator

--- Calculator/Models/NumberValidator.cs
+++ Calculator/Models/NumberValidator.cs
@@ -1,11 +1,25 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace Calculator.Models;
 
 public static class NumberValidator
 {
-    public static bool ValidateNumber(string number, int numberSystem)
+    private const string Alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+    private const int MinAllowedBase = 2;
+    private static readonly int MaxAllowedBase = Alphabet.Length;
+
+    public static bool ValidateNumber(string number, int numberBase)
     {
-        throw new NotImplementedException();
+        if (numberBase > MaxAllowedBase || numberBase < MinAllowedBase)
+            throw new ArgumentOutOfRangeException(nameof(numberBase),
+                $"Number base must be from {MinAllowedBase} to {MaxAllowedBase}");
+
+        return GetValidationRegex(numberBase).IsMatch(number);
     }
+
+    private static string GetAllowedSymbols(int numberBase) => Alphabet[..numberBase];
+
+    private static Regex GetValidationRegex(int numberBase) =>
+        new($"^[{GetAllowedSymbols(numberBase)}]+$", RegexOptions.IgnoreCase);
 }
