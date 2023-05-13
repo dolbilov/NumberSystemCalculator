27e81f3aeae0798dfde596b31dde7517be476ec7
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-13 19:43:42 +0300

    feat: implement INotifyDataErrorInfo

--- Calculator/ViewModels/MainWindowViewModel.cs
+++ Calculator/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,17 @@
-﻿using ReactiveUI;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using ReactiveUI;
 
 namespace Calculator.ViewModels;
 
-public class MainWindowViewModel : ViewModelBase
+public class MainWindowViewModel : ViewModelBase, INotifyDataErrorInfo
 {
     #region Fields
 
+    private readonly Dictionary<string, List<ValidationResult>> _validationErrors = new();
     private string? _firstNumber;
     private int _firstNumericSystem = 10;
     private string? _secondNumber;
@@ -45,4 +51,37 @@ public class MainWindowViewModel : ViewModelBase
     }
 
     #endregion
+
+    public IEnumerable GetErrors(string? propertyName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool HasErrors => _validationErrors.Count > 0;
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    private void AddError(string propertyName, string errorMessage)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(propertyName);
+
+        if (_validationErrors.TryGetValue(propertyName, out var errors))
+            errors.Add(new ValidationResult(errorMessage));
+        else
+            _validationErrors.Add(propertyName, new List<ValidationResult> { new(errorMessage) });
+
+        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        this.RaisePropertyChanged(nameof(HasErrors));
+    }
+
+    private void ClearErrors(string propertyName = "")
+    {
+        if (string.IsNullOrEmpty(propertyName))
+            _validationErrors.Clear();
+
+        _validationErrors.Remove(propertyName);
+
+        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        this.RaisePropertyChanged(nameof(HasErrors));
+    }
+    
 }
