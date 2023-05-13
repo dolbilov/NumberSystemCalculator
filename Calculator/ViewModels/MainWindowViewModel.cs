636785ffbb2eb49ffd9e51ab2d77bdfb33384b0f
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-13 16:04:36 +0300

    chore: setup View and ViewModel

--- Calculator/ViewModels/MainWindowViewModel.cs
+++ Calculator/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,48 @@
-﻿namespace Calculator.ViewModels;
+﻿using ReactiveUI;
+
+namespace Calculator.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    #region Fields
+
+    private string? _firstNumber;
+    private int _firstNumericSystem = 10;
+    private string? _secondNumber;
+    private int _secondNumericSystem = 10;
+
+    #endregion
+
+
+    public MainWindowViewModel()
+    {
+    }
+
+    #region Properties
+
+    public string? FirstNumber
+    {
+        get => _firstNumber;
+        set => this.RaiseAndSetIfChanged(ref _firstNumber, value);
+    }
+
+    public int FirstNumericSystem
+    {
+        get => _firstNumericSystem;
+        set => this.RaiseAndSetIfChanged(ref _firstNumericSystem, value);
+    }
+
+    public string? SecondNumber
+    {
+        get => _secondNumber;
+        set => this.RaiseAndSetIfChanged(ref _secondNumber, value);
+    }
+
+    public int SecondNumericSystem
+    {
+        get => _secondNumericSystem;
+        set => this.RaiseAndSetIfChanged(ref _secondNumericSystem, value);
+    }
+
+    #endregion
 }
