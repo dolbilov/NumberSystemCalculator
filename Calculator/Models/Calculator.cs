235a6ccfd5a8b64ca48d93fc965d11a684c38de3
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-14 11:17:13 +0300

    feat!: move Alphabet to Calculator and finish transformation to another base

--- Calculator/Models/Calculator.cs
+++ Calculator/Models/Calculator.cs
@@ -1,14 +1,62 @@
 using System;
+using System.Linq;
+using System.Text;
 
 namespace Calculator.Models;
 
 public static class Calculator
 {
+    public const string Alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+
     public static string TransferNumberToAnotherBase(string input, int inputBase, int resultBase)
     {
         if (inputBase == resultBase)
             return input;
 
-        throw new NotImplementedException();
+        try
+        {
+            var decimalValue = ConvertToDecimalBase(input, inputBase);
+            return ConvertFromDecimalBase(decimalValue, resultBase);
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+
+    private static long ConvertToDecimalBase(string input, int inputBase)
+    {
+        long res = 0;
+        var len = input.Length;
+
+        for (var i = 0; i < len; i++)
+        {
+            var symbol = input[i];
+            var value = Alphabet.IndexOf(symbol);
+
+            res += value * (long)Math.Pow(inputBase, len - i - 1);
+        }
+
+        return res;
+    }
+
+    private static string ConvertFromDecimalBase(long inputNumber, int resultBase)
+    {
+        StringBuilder sb = new();
+
+        while (inputNumber > 0)
+        {
+            sb.Append(inputNumber % resultBase);
+            inputNumber /= resultBase;
+        }
+
+        return ReverseString(sb.ToString());
+    }
+
+    private static string ReverseString(string s)
+    {
+        var arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
     }
 }
--- Calculator/Models/NumberValidator.cs
+++ Calculator/Models/NumberValidator.cs
@@ -5,9 +5,8 @@ namespace Calculator.Models;
 
 public static class NumberValidator
 {
-    private const string Alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
     private const int MinAllowedBase = 2;
-    private static readonly int MaxAllowedBase = Alphabet.Length;
+    private static readonly int MaxAllowedBase = Calculator.Alphabet.Length;
 
     public static bool ValidateNumber(string number, int numberBase)
     {
@@ -18,7 +17,7 @@ public static class NumberValidator
         return GetValidationRegex(numberBase).IsMatch(number);
     }
 
-    private static string GetAllowedSymbols(int numberBase) => Alphabet[..numberBase];
+    private static string GetAllowedSymbols(int numberBase) => Calculator.Alphabet[..numberBase];
 
     private static Regex GetValidationRegex(int numberBase) =>
         new($"^[{GetAllowedSymbols(numberBase)}]+$", RegexOptions.IgnoreCase);
